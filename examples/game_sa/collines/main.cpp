c8414152408fed54e175efa28a0a112c474983c0
[-=KILL MAN=-] <mctesting1@yandex.ru>
2026-07-07 05:11:14 +0300

    Fix ColLines example

--- examples/game_sa/collines/main.cpp
+++ examples/game_sa/collines/main.cpp
@@ -7,9 +7,12 @@
 #include <aml-psdk/game_sa/other/Pools.h>
 #include <aml-psdk/game_sa/base/ColModel.h>
 
+#include <aml-psdk/renderware/RwRender.h>
+
 MYMOD(net.rusjj.psdkexample.collines, AML PSDK Example: ColLines, 1.0, FoxiruFoxcatIsDW)
 
 // FoxiruFoxcatIsDW
+const float g_fDrawRadius = 150.0f;
 void DrawEntityCollision(CEntity* entity)
 {
     if(!entity->GetIsOnScreen()) return;
@@ -28,55 +31,118 @@ void DrawEntityCollision(CEntity* entity)
     uint16_t numTriangles =     colData->m_nNumTriangles;
     CVector* pVertices =        colData->m_pVertices;
     CColTriangle* pTriangles =  colData->m_pTriangles;
+    
+    if(!pTriangles || !pVertices) return;
 
-    //                     0x R G B A
+    //                    0x R G B A
     const uint32_t color = 0x00FF00FF;
-
     for(int i = 0; i < numTriangles; ++i)
     {
         CColTriangle* currentTri = &pTriangles[i];
-        uint32_t vA = currentTri->m_nVertA;
-        uint32_t vB = currentTri->m_nVertB;
-        uint32_t vC = currentTri->m_nVertC;
+        int32_t vA = currentTri->m_nVertA;
+        int32_t vB = currentTri->m_nVertB;
+        int32_t vC = currentTri->m_nVertC;
 
         CVector v1 = pVertices[vA];
         CVector v2 = pVertices[vB];
         CVector v3 = pVertices[vC];
-        
-        float temp;
-        temp = v1.y; v1.y = v1.z; v1.z = temp;
-        temp = v2.y; v2.y = v2.z; v2.z = temp;
-        temp = v3.y; v3.y = v3.z; v3.z = temp;
 
         v1 = matrix->TransformPos(v1);
         v2 = matrix->TransformPos(v2);
         v3 = matrix->TransformPos(v3);
 
         CLines::RenderLineWithClipping(v1.x, v1.y, v1.z, v2.x, v2.y, v2.z, color, color);
-        CLines::RenderLineWithClipping(v1.x, v1.y, v1.z, v3.x, v3.y, v3.z, color, color);
         CLines::RenderLineWithClipping(v2.x, v2.y, v2.z, v3.x, v3.y, v3.z, color, color);
+        CLines::RenderLineWithClipping(v3.x, v3.y, v3.z, v1.x, v1.y, v1.z, color, color);
     }
 }
 
 ON_MOD_LOAD()
 {
+    Events::pedRenderEvent += [](CPed* ped)
+    {
+        
+    };
     Events::drawingEvent += []()
     {
         CPlayerPed* player = FindPlayerPed(-1);
         if(!player) return;
 
-        CBuildingPool* buildingPool = CPools::ms_pBuildingPool;
-        if(buildingPool)
+        RwRenderStateSet(rwRENDERSTATEZWRITEENABLE, (void*)true);
+        RwRenderStateSet(rwRENDERSTATEVERTEXALPHAENABLE, (void*)true);
+        RwRenderStateSet(rwRENDERSTATESRCBLEND, (void*)rwBLENDSRCALPHA);
+        RwRenderStateSet(rwRENDERSTATEDESTBLEND, (void*)rwBLENDINVSRCALPHA);
+        RwRenderStateSet(rwRENDERSTATETEXTURERASTER, (void*)NULL);
+
+        #define DRAW_RAD_SQR (g_fDrawRadius*g_fDrawRadius)
         {
-            int size = buildingPool->GetSize();
+        auto curPool = CPools::ms_pVehiclePool;
+        if(curPool)
+        {
+            int size = curPool->GetSize();
             for(int i = 0; i < size; ++i)
             {
-                CBuilding* building = buildingPool->GetAt(i);
-                if(building && building->GetDistanceSqrTo(player) > 100*100)
+                CEntity* ent = curPool->GetAt(i);
+                if(ent && ent->GetDistanceSqrTo(player) < DRAW_RAD_SQR)
                 {
-                    DrawEntityCollision(building);
+                    DrawEntityCollision(ent);
                 }
             }
         }
+        }
+        
+        {
+        auto curPool = CPools::ms_pPedPool;
+        if(curPool)
+        {
+            int size = curPool->GetSize();
+            for(int i = 0; i < size; ++i)
+            {
+                CEntity* ent = curPool->GetAt(i);
+                if(ent && ent->GetDistanceSqrTo(player) < DRAW_RAD_SQR)
+                {
+                    DrawEntityCollision(ent);
+                }
+            }
+        }
+        }
+        
+        {
+        auto curPool = CPools::ms_pBuildingPool;
+        if(curPool)
+        {
+            int size = curPool->GetSize();
+            for(int i = 0; i < size; ++i)
+            {
+                CEntity* ent = curPool->GetAt(i);
+                if(ent && ent->GetDistanceSqrTo(player) < DRAW_RAD_SQR)
+                {
+                    DrawEntityCollision(ent);
+                }
+            }
+        }
+        }
+        
+        {
+        auto curPool = CPools::ms_pObjectPool;
+        if(curPool)
+        {
+            int size = curPool->GetSize();
+            for(int i = 0; i < size; ++i)
+            {
+                CEntity* ent = curPool->GetAt(i);
+                if(ent && ent->GetDistanceSqrTo(player) < DRAW_RAD_SQR)
+                {
+                    DrawEntityCollision(ent);
+                }
+            }
+        }
+        }
+        
+        RwRenderStateSet(rwRENDERSTATESRCBLEND, (void*)5);
+        RwRenderStateSet(rwRENDERSTATEDESTBLEND, (void*)6);
+        RwRenderStateSet(rwRENDERSTATEVERTEXALPHAENABLE, (void*)false);
+        RwRenderStateSet(rwRENDERSTATEZWRITEENABLE, (void*)true);
+        RwRenderStateSet(rwRENDERSTATEZTESTENABLE, (void*)true);
     };
 }
